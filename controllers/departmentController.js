6fbfbb22825d28077f0e210453971a5d2552c83b
abhishek-yadav-2006 <ydvabhishek447@gmail.com>
2026-08-03 20:13:57 +0530

    create and get departments

---
+++ controllers/departmentController.js
@@ -0,0 +1,54 @@
+import {
+    createDepartment,
+    getDepartments,
+    getDepartmentByName,
+    getDepartmentById,
+    updateDepartment,
+    deleteDepartment,
+    countEmployeesInDepartment
+} from "../models/departmentModel.js";
+
+export const createDepartmentController = async (req, res) => {
+    try {
+        const { departmentName } = req.body;
+
+        if (!departmentName) {
+            return res.status(400).json({
+                message: "Department name is required"
+            });
+        }
+
+        const department = await getDepartmentByName(departmentName);
+
+        if (department.length > 0) {
+            return res.status(400).json({
+                message: "Department already exists"
+            });
+        }
+
+        await createDepartment(departmentName);
+
+        return res.status(201).json({
+            message: "Department created successfully"
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+export const getDepartmentsController = async (req, res) => {
+    try {
+        const departments = await getDepartments();
+
+        return res.status(200).json(departments);
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
